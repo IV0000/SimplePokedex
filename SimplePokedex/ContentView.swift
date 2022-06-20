6fb7a7b73a004ebdd643b407fa219e48b391624f
Ivan <keranamv@gmail.com>
2022-06-20 23:52:17 +0200

    Initial Commit

---
+++ SimplePokedex/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SimplePokedex
+//
+//  Created by Ivan Voloshchuk on 20/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SimplePokedex/SimplePokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimplePokedexApp.swift
+//  SimplePokedex
+//
+//  Created by Ivan Voloshchuk on 20/06/22.
+//
+
+import SwiftUI
+
+@main
+struct SimplePokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
